799589af53abcd8f0eac2c44b5731af37bab747f
Mainulhasan05 <mdrifatbd5@gmail.com>
2021-10-01 10:41:24 +0600

    as

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function createSection(){
+    let noOfCourse=document.getElementById("no-of-course").value;
+    console.log(noOfCourse);
+    if(noOfCourse<=5){
+        let item=document.querySelectorAll(".hide");
+        console.log(item);
+        for(let i=0; i<item.length; i++){
+            item[i].style.display="none";
+        }
+        for(let i=0; i<noOfCourse; i++){
+            item[i].style.display="block";
+        }
+    }
+    else{
+        alert("Can't take more than 5 courses.")
+    }
+
+}
+
 var total=3;
 
 function validityCheck(){
