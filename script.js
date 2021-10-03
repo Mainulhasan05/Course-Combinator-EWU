8742a97dbce32468a26c49079a5113c6309b1927
Mainulhasan05 <mdrifatbd5@gmail.com>
2021-10-03 15:52:53 +0600

    asds

--- script.js
+++ script.js
@@ -354,7 +354,7 @@ function myFunction(param) {
 
 /* for lab needed indexs {2,3} ,{6,7}, {10,11} labobj*/
 }
-
+var numberOfRowPerCourse=[];
 function createNew(){
     if(total==3){
         var neww=document.getElementById("new1");
